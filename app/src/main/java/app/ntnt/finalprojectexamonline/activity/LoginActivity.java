397b64f76bcf1eceb8431d61b361a97ec192e378
MDung0807 <dominhdung21082002@gmail.com>
2023-04-15 20:08:44 +0700

    add get all subject for student

--- app/src/main/java/app/ntnt/finalprojectexamonline/activity/LoginActivity.java
+++ app/src/main/java/app/ntnt/finalprojectexamonline/activity/LoginActivity.java
@@ -1,11 +1,8 @@
 package app.ntnt.finalprojectexamonline.activity;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,16 +13,12 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
-import app.ntnt.finalprojectexamonline.R;
 import app.ntnt.finalprojectexamonline.databinding.ActivityLoginBinding;
 import app.ntnt.finalprojectexamonline.model.request.LoginRequest;
 import app.ntnt.finalprojectexamonline.model.response.AuthResponse;
 import app.ntnt.finalprojectexamonline.services.BaseAPIService;
-import app.ntnt.finalprojectexamonline.services.ILoginService;
+import app.ntnt.finalprojectexamonline.services.IAuthService;
+import app.ntnt.finalprojectexamonline.utils.ContextUtil;
 import app.ntnt.finalprojectexamonline.utils.SharedPrefManager;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -45,6 +38,7 @@ public class LoginActivity extends AppCompatActivity {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         binding =ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        ContextUtil.context = getApplicationContext();
 //        //Ánh xạ
         init();
         //Chuyển trang đăng kí
@@ -64,15 +58,14 @@ public class LoginActivity extends AppCompatActivity {
                 String username = binding.edtUsername.getText().toString();
                 String password = binding.edtPassword.getText().toString();
                 LoginRequest loginRequest = new LoginRequest(username, password);
-                BaseAPIService.createService(ILoginService.class).login(loginRequest)
+                BaseAPIService.createService(IAuthService.class).login(loginRequest)
                         .enqueue(new Callback<AuthResponse>() {
                             @Override
                             public void onResponse(Call<AuthResponse> call, Response<AuthResponse> response) {
                                 progressBar.setVisibility(View.INVISIBLE);
                                 if (response.body()!=null){
-                                    Log.d("true", response.message().toString());
-                                    Log.d("token", response.body().getToken());
                                     SharedPrefManager.getInstance(getApplicationContext()).saveAuthToken(response.body());
+                                    SharedPrefManager.getInstance(getApplicationContext()).saveUser(response.body().getUsername());
                                     Toast.makeText(LoginActivity.this, "Đăng nhập thành công", Toast.LENGTH_LONG).show();
                                     Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
                                     startActivity(intent);
@@ -82,7 +75,6 @@ public class LoginActivity extends AppCompatActivity {
                                 }
 
                             }
-
                             @Override
                             public void onFailure(Call<AuthResponse> call, Throwable t) {
                                 Toast.makeText(LoginActivity.this, "Đăng nhập Thất bại", Toast.LENGTH_LONG).show();
--- app/src/main/java/app/ntnt/finalprojectexamonline/model/entites/Topic.java
+++ app/src/main/java/app/ntnt/finalprojectexamonline/model/entites/Topic.java
@@ -1,23 +1,43 @@
 package app.ntnt.finalprojectexamonline.model.entites;
 
 public class Topic {
-    private int id;
+    private Long id;
     private String name;
+    private String image;
+    private int status;
 
-    public Topic(int id, String name) {
+    public Topic(Long id, String name, String image, int status) {
         this.id = id;
         this.name = name;
+        this.image = image;
+        this.status = status;
     }
 
-    public Topic() {
-    }
-
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Topic() {
     }
 
     public String getName() {
